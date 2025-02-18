2cabbbccfcb817990d894f888d9760f14f47ef54
zdmiller12 <zdmiller12@gmail.com>
2025-02-18 03:49:56 -0500

    bump

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,10 +12,8 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends(
     "next/core-web-vitals",
-    "next/typescript",
-    "plugin:prettier/recommended",
+    "next/typescript"
   ),
-  "plugin:mdx/recommended",
 ];
 
 export default eslintConfig;
